1a5933a3b5e089a0b594ba94129c2a462ec26554
unknown <roy.duenas@mobilemoney.ph>
2022-11-18 19:10:53 +0800

    name

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker stop asc_screener_fe
-docker rm asc_screener_fe
-docker rmi asc_screener_fe_v1
+docker stop asc_reviewer_fe
+docker rm asc_reviewer_fe
+docker rmi asc_reviewer_fe_v1
 
-docker build -t asc_screener_fe_v1 .
-docker run -d -p 1318:1318 --name asc_screener_fe asc_screener_fe_v1
+docker build -t asc_reviewer_fe_v1 .
+docker run -d -p 1318:1318 --name asc_reviewer_fe asc_reviewer_fe_v1
 docker image prune -f
 
